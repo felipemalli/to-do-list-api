4a7d7ec25164cac8c806ca2c1dd8d9914ac7f8c5
FelipeVahia <felipevahia@gmail.com>
2022-06-22 02:13:26 -0300

    feat(api): install eslint

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,13 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, {
+  Application, Request, Response, NextFunction,
+} from 'express';
 
 const app: Application = express();
 
-const miniDatabase = [{ name: 'Roberto', age: 26}, { name: 'Luciana', age: 56 }]
+const miniDatabase = [{ name: 'Roberto', age: 26 }, { name: 'Luciana', age: 56 }];
 
-app.get('/', (req: Request, res: Response, next: NextFunction, ) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send(miniDatabase);
-}) 
+});
 
-app.listen(5000, () => console.log('Server runnning'))
+app.listen(5000, () => console.log('Server runnning'));
